28169917e7a059d4d11945677799e34ee0681bb1
yurim22 <dek1313@naver.com>
2024-04-16 19:35:01 +0900

    [chore] 화면 라우터 설정(메인/로그인/회원가입)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import './reset.scss';
+import { Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Main from './pages/Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/SignIn/SignInPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignInPage = () => {
+    return (
+        <>로그인 페이지 입니다.</>
+    )
+}
+
+export default SignInPage;
