d41795fbc2ec75f2433b378a0598401037eed6bc
22dongeun <22dongeun@gmail.com>
2024-04-30 01:12:09 +0900

    [layout_component] pretter 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Main from './pages/Main'
-import { Layout } from './components/Layout'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Main from './pages/Main';
+import { Layout } from './components/Layout';
 
 function App() {
-  return (
-    <Layout>
-      <div className="wrap">
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-        </Routes>
-      </div>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<div className="wrap">
+				<Routes>
+					<Route path="/" element={<Main />} />
+					<Route path="/sign-in" element={<SignIn />} />
+					<Route path="/sign-up" element={<SignUp />} />
+				</Routes>
+			</div>
+		</Layout>
+	);
 }
 
-export default App
+export default App;
