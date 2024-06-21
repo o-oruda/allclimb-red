ebf154dd5dee244aba036a72d6f928bdb7fe77e6
22dongeun <22dongeun@gmail.com>
2024-06-21 21:38:27 +0900

    StepBar 공통 컴포넌트 추가

--- src/pages/SignUp/SignUpPage.tsx
+++ src/pages/SignUp/SignUpPage.tsx
@@ -1,203 +1,207 @@
-import React from "react";
+import React, { useState } from 'react';
 
-import styles from "./SignUpPage.module.scss";
-import classNames from "classnames/bind";
-import NavBar from "../../components/NavBar/NavBar";
-import Search from "../../components/Search/Search";
-import BottomModal from "../../components/Modal/BottomModal/BottomModal";
+import styles from './SignUpPage.module.scss';
+import classNames from 'classnames/bind';
+import NavBar from '../../components/NavBar/NavBar';
+import Search from '../../components/Search/Search';
+import BottomModal from '../../components/Modal/BottomModal/BottomModal';
+import StepBar from '../../components/StepBar/StepBar';
 
 const cx = classNames.bind(styles);
 
 const SignUpPage = () => {
-    return (
-        <>
-            <div className={cx('sign-up')}>
-                <NavBar/>
-                <button type="button" className={cx('sign-up__skip-button')}>
-                    건너뛰기
-                </button>
+	const [step, setStep] = useState(1);
 
-                <div className={cx('sign-up-page')}>
-                    <h3 className="blind">질문 단계</h3>
-                    {/* TODO [F/E] - 선택시 sign-up-page__step--active 추가 */}
-                    <div className={cx('sign-up-page__step','sign-up-page__step--active')}>
-                        <span className="blind">1단계</span>
-                    </div>
+	return (
+		<>
+			<div className={cx('sign-up')}>
+				<NavBar />
+				<button type="button" className={cx('sign-up__skip-button')}>
+					건너뛰기
+				</button>
 
-                    <div className={cx('sign-up-page__step')}>
-                        <span className="blind">2단계</span>
-                    </div>
+				<StepBar hiddenTitle="질문 단계" step={step} totalStep={3} />
 
-                    <div className={cx('sign-up-page__step')}>
-                        <span className="blind">3단계</span>
-                    </div>
-                </div>
+				<h2 className={cx('sign-up__title')}>
+					평소 관심있던
+					<br />
+					클라이밍장은 어디인가요?
+				</h2>
 
-                <h2 className={cx('sign-up__title')}>
-                    평소 관심있던<br/>클라이밍장은 어디인가요?
-                </h2>
+				<div className={cx('sign-up-center')}>
+					<ul className={cx('sign-up-center__list')}>
+						<li className={cx('sign-up-center__item')}>
+							{/* TODO [F/E] - 선택시 sign-up-center__button--active 추가 */}
+							<button
+								type="button"
+								className={cx(
+									'sign-up-center__button',
+									'sign-up-center__button--active',
+								)}
+							>
+								신촌담장
+							</button>
+						</li>
 
-                <div className={cx('sign-up-center')}>
-                    <ul className={cx('sign-up-center__list')}>
-                        <li className={cx('sign-up-center__item')}>
-                            {/* TODO [F/E] - 선택시 sign-up-center__button--active 추가 */}
-                            <button
-                                type="button" 
-                                className={cx('sign-up-center__button', 'sign-up-center__button--active')}
-                            >
-                                신촌담장
-                            </button>
-                        </li>
+						<li className={cx('sign-up-center__item')}>
+							<button
+								type="button"
+								className={cx('sign-up-center__button')}
+							>
+								온사이트 클라이밍
+							</button>
+						</li>
 
-                        <li className={cx('sign-up-center__item')}>
-                            <button
-                                type="button" 
-                                className={cx('sign-up-center__button')}
-                            >
-                                온사이트 클라이밍
-                            </button>
-                        </li>
+						<li className={cx('sign-up-center__item')}>
+							<button
+								type="button"
+								className={cx('sign-up-center__button')}
+							>
+								락랜드클라이밍
+							</button>
+						</li>
+					</ul>
 
-                        <li className={cx('sign-up-center__item')}>
-                            <button
-                                type="button" 
-                                className={cx('sign-up-center__button')}
-                            >
-                                락랜드클라이밍
-                            </button>
-                        </li>
-                    </ul>
-                    
-                    <button
-                        type="button" 
-                        className={cx('sign-up-center__button', 'sign-up-center__button--plus')}
-                    >
-                        직접추가
-                    </button>
-                </div>
+					<button
+						type="button"
+						className={cx(
+							'sign-up-center__button',
+							'sign-up-center__button--plus',
+						)}
+					>
+						직접추가
+					</button>
+				</div>
 
-                {/* TODO [F/E] - 검색 */}
-                <div className={cx('sign-up-search')}>
-                    <h3 className={cx('sign-up-search__title')}>
-                        관심있는 클라이밍 장을<br/>추가해보세요!
-                    </h3>
+				{/* TODO [F/E] - 검색 */}
+				<div className={cx('sign-up-search')}>
+					<h3 className={cx('sign-up-search__title')}>
+						관심있는 클라이밍 장을
+						<br />
+						추가해보세요!
+					</h3>
 
-                    <div className={cx('sign-up-search__box')}>
-                        <Search/>
-                    </div>
-                </div>
+					<div className={cx('sign-up-search__box')}>
+						<Search />
+					</div>
+				</div>
 
-                {/* TODO [F/E] - 활동지역 선택  */}
-                <h2 className={cx('sign-up__title')}>
-                    주로 활동하는 지역은<br/>어디인가요?
-                </h2>
+				{/* TODO [F/E] - 활동지역 선택  */}
+				<h2 className={cx('sign-up__title')}>
+					주로 활동하는 지역은
+					<br />
+					어디인가요?
+				</h2>
 
-                <div className={cx('sign-up-area')}>
-                    <ul className={cx('sign-up-area__list')}>
-                        <li className={cx('sign-up-area__item')}>
-                            {/* TODO [F/E] - 선택시 sign-up-area__button--active 추가 */}
-                            <button
-                                type="button" 
-                                className={cx('sign-up-area__button', 'sign-up-area__button--active')}
-                            >
-                                서울
-                            </button>
-                        </li>
+				<div className={cx('sign-up-area')}>
+					<ul className={cx('sign-up-area__list')}>
+						<li className={cx('sign-up-area__item')}>
+							{/* TODO [F/E] - 선택시 sign-up-area__button--active 추가 */}
+							<button
+								type="button"
+								className={cx(
+									'sign-up-area__button',
+									'sign-up-area__button--active',
+								)}
+							>
+								서울
+							</button>
+						</li>
 
-                        <li className={cx('sign-up-area__item')}>
-                            <button
-                                type="button" 
-                                className={cx('sign-up-area__button')}
-                            >
-                                경기
-                            </button>
-                        </li>
+						<li className={cx('sign-up-area__item')}>
+							<button
+								type="button"
+								className={cx('sign-up-area__button')}
+							>
+								경기
+							</button>
+						</li>
 
-                        <li className={cx('sign-up-area__item')}>
-                            <button
-                                type="button" 
-                                className={cx('sign-up-area__button')}
-                            >
-                                강원
-                            </button>
-                        </li>
+						<li className={cx('sign-up-area__item')}>
+							<button
+								type="button"
+								className={cx('sign-up-area__button')}
+							>
+								강원
+							</button>
+						</li>
 
-                        <li className={cx('sign-up-area__item')}>
-                            <button
-                                type="button" 
-                                className={cx('sign-up-area__button')}
-                            >
-                                충청
-                            </button>
-                        </li>
+						<li className={cx('sign-up-area__item')}>
+							<button
+								type="button"
+								className={cx('sign-up-area__button')}
+							>
+								충청
+							</button>
+						</li>
 
-                        <li className={cx('sign-up-area__item')}>
-                            <button
-                                type="button" 
-                                className={cx('sign-up-area__button')}
-                            >
-                                전라
-                            </button>
-                        </li>
+						<li className={cx('sign-up-area__item')}>
+							<button
+								type="button"
+								className={cx('sign-up-area__button')}
+							>
+								전라
+							</button>
+						</li>
 
-                        <li className={cx('sign-up-area__item')}>
-                            <button
-                                type="button" 
-                                className={cx('sign-up-area__button')}
-                            >
-                                경상
-                            </button>
-                        </li>
+						<li className={cx('sign-up-area__item')}>
+							<button
+								type="button"
+								className={cx('sign-up-area__button')}
+							>
+								경상
+							</button>
+						</li>
 
-                        <li className={cx('sign-up-area__item')}>
-                            <button
-                                type="button" 
-                                className={cx('sign-up-area__button')}
-                            >
-                                부산
-                            </button>
-                        </li>
+						<li className={cx('sign-up-area__item')}>
+							<button
+								type="button"
+								className={cx('sign-up-area__button')}
+							>
+								부산
+							</button>
+						</li>
 
-                        <li className={cx('sign-up-area__item')}>
-                            <button
-                                type="button" 
-                                className={cx('sign-up-area__button')}
-                            >
-                                제주
-                            </button>
-                        </li>
+						<li className={cx('sign-up-area__item')}>
+							<button
+								type="button"
+								className={cx('sign-up-area__button')}
+							>
+								제주
+							</button>
+						</li>
 
-                        <li className={cx('sign-up-area__item')}>
-                            <button
-                                type="button" 
-                                className={cx('sign-up-area__button')}
-                            >
-                                전국
-                            </button>
-                        </li>
-                    </ul>
+						<li className={cx('sign-up-area__item')}>
+							<button
+								type="button"
+								className={cx('sign-up-area__button')}
+							>
+								전국
+							</button>
+						</li>
+					</ul>
 
-                    {/* TODO [F/E] - 날짜 선택  */}
-                    <h2 className={cx('sign-up__title')}>
-                        클라이밍,<br/>언제 시작했나요?
-                    </h2>
-                </div>
+					{/* TODO [F/E] - 날짜 선택  */}
+					<h2 className={cx('sign-up__title')}>
+						클라이밍,
+						<br />
+						언제 시작했나요?
+					</h2>
+				</div>
 
-                {/* TODO [F/E] - 하나라도 선택시 sign-up-area-bottom--active 추가 */}
-                <div className={cx('sign-up-bottom', 'sign-up-bottom--active')}>
-                    <button 
-                        type="button"
-                        className={cx('sign-up-bottom__button')}
-                    >
-                        다음
-                    </button>
-                </div>
-            </div>
-            <BottomModal>
-                d
-            </BottomModal>
-        </>
-    )
-}
+				{/* TODO [F/E] - 하나라도 선택시 sign-up-area-bottom--active 추가 */}
+				<div className={cx('sign-up-bottom', 'sign-up-bottom--active')}>
+					<button
+						type="button"
+						className={cx('sign-up-bottom__button')}
+					>
+						다음
+					</button>
+				</div>
+			</div>
+			<BottomModal>d</BottomModal>
+		</>
+	);
+};
 
 export default SignUpPage;
